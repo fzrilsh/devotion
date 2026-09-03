bebedb0cbb1979d389918153c15299db205587d1
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-09-03 11:21:33 +0700

    fix(frontend): perbaiki path aset marker Leaflet
    Ikon default Leaflet dihitung relatif terhadap halaman sehingga browser
    meminta /profile/marker-shadow.png. Ketiga PNG marker kini diimpor
    lewat Vite dan dipasang eksplisit pada Marker di LocationMap dan
    LocationPicker.

--- frontend/src/components/common/LocationMap.tsx
+++ frontend/src/components/common/LocationMap.tsx
@@ -1,5 +1,6 @@
 import "leaflet/dist/leaflet.css";
 
+import { defaultMarkerIcon } from "@lib/leafletIcons";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 type LocationMapProps = {
@@ -14,7 +15,7 @@ export default function LocationMap({ latitude, longitude, label = "Lokasi" }: L
             <MapContainer center={[latitude, longitude]} zoom={15} scrollWheelZoom={false} className="h-full w-full z-10">
                 <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-                <Marker position={[latitude, longitude]}>
+                <Marker position={[latitude, longitude]} icon={defaultMarkerIcon}>
                     <Popup>{label}</Popup>
                 </Marker>
             </MapContainer>
